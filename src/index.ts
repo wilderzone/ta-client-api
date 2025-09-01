c3660745197fb8de9d8726a36ee0af21b483a4b0
Giga <gigabyte5671@gmail.com>
2025-09-02 09:17:33 +1200

    Export the `ChildProcess` interface

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export type { RuntimeAdapter } from './adapter.js';
+export type { ChildProcess, RuntimeAdapter } from './adapter.js';
 export { Config } from './config.js';
 export { GameClient } from './client.js';
 export type { InternalMapName } from './maps.js';
