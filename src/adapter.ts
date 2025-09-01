aa171dc614ed646fb4db2793ea3d5d6b223f87fb
Giga <gigabyte5671@gmail.com>
2025-09-02 00:01:35 +1200

    Define a `RuntimeAdapter` interface

---
+++ src/adapter.ts
@@ -0,0 +1,19 @@
+type ChildProcessEventHandler<T, A = Array<any>> = (event: T, callback: (args: A) => void) => void;
+type ChildProcessEvent = 'data' | 'exit';
+
+export interface ChildProcess {
+	/** Indicates whether it is still possible to send/receive messages to/from the child process. */
+	connected: boolean;
+	/** Listen to events emitted by the child process. */
+	on: ChildProcessEventHandler<ChildProcessEvent, string>;
+	/** Start the child process. */
+	start: () => Promise<void>;
+	/** Stop the child process. */
+	stop: () => Promise<void>;
+}
+
+export interface RuntimeAdapter {
+	process: {
+		spawn: (command: string, args?: string[]) => ChildProcess;
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export type { RuntimeAdapter } from './adapter.js';
 export { Config } from './config.js';
 export { GameClient } from './client.js';
 export type { InternalMapName } from './maps.js';
